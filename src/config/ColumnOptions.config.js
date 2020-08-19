ee8e476cc9f9322f90941bd251a24c628ae4362c
KaryaSmd <karyasmd@KaryaSmds-Mac-mini.local>
2020-08-19 13:38:53 +0300

    new column hide

--- src/config/ColumnOptions.config.js
+++ src/config/ColumnOptions.config.js
@@ -11,9 +11,9 @@ export default {
       {
         key: "dealerSubCode",
       },
-      {
-        key: "C-DBS",
-      },
+      // {
+      //   key: "C-DBS",
+      // },
       {
         key: "regionCode",
       },
