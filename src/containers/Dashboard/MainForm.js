18099f905554b6d39793dd7777c7418c788254d3
KaryaSmd <karyasmd@KaryaSmd.local>
2020-12-01 14:25:11 +0300

    main form filter

--- src/containers/Dashboard/MainForm.js
+++ src/containers/Dashboard/MainForm.js
@@ -402,7 +402,7 @@ const MainForm = () => {
                   showSearch
                   mode="multiple"
                   dropdownMatchSelectWidth={500}
-                  style={{ width: '100%' }}
+                  style={{ marginBottom: '8px', width: '100%' }}
                   placeholder="Bayi seçiniz"
                   optionFilterProp="children"
                   value={dealerCodes}
@@ -414,12 +414,11 @@ const MainForm = () => {
                   {lookupDealerChildren}
                 </Select>
               </Col>
-              <Col align={'right'} span={newView !== 'MobileView' ? 1 : 0} md={newView !== 'MobileView' ? null : 12} sm={newView !== 'MobileView' ? null : 12} xs={newView !== 'MobileView' ? null : 24}>
-              
+              <Col span={newView !== 'MobileView' ? 1 : 0} md={newView !== 'MobileView' ? null : 12} sm={newView !== 'MobileView' ? null : 12} xs={newView !== 'MobileView' ? null : 24}>
+          </Col>
               <Button style={{ marginBottom: '8px',  width: newView !== 'MobileView' ? '125px' : '100%' }} type="primary" loading={iconLoading} onClick={dataSearch} >
                 {<IntlMessages id="forms.button.label_Search" />}
               </Button>
-              </Col>
             </Row>
           </Panel>
         </Collapse>
