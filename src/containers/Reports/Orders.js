8b140724db009efb32b390090f2dfa56cd985711
ugur-camoglu <ugur.camoglu@karyasmd.com>
2021-03-04 11:07:56 +0300

    ürün arama etiket ismi değişikliği

--- src/containers/Reports/Orders.js
+++ src/containers/Reports/Orders.js
@@ -785,7 +785,7 @@ const OrdersReport = () => {
                                 </Select>
                             </Col>
                             <Col span={view !== 'MobileView' ? 6 : 0} md={view !== 'MobileView' ? null : 12} sm={view !== 'MobileView' ? null : 12} xs={view !== 'MobileView' ? null : 24}>
-                                <Input size="small" placeholder="Ürün Adı, Sipariş No ... giriniz" style={{ marginBottom: '8px', width: view !== 'MobileView' ? '250px' : '100%' }} value={searchKey} onKeyDown={keyPress} onChange={event => setSearchKey(event.target.value)} />
+                                <Input size="small" placeholder="Sipariş No ... giriniz" style={{ marginBottom: '8px', width: view !== 'MobileView' ? '250px' : '100%' }} value={searchKey} onKeyDown={keyPress} onChange={event => setSearchKey(event.target.value)} />
 
                             </Col>
                         </Row>
