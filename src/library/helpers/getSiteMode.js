9570b0cefea1010da8859cdc4bdc010a9047cfd7
ugur-camoglu <ugur.camoglu@karyasmd.com>
2021-10-21 16:09:05 +0300

    side mode change

--- src/library/helpers/getSiteMode.js
+++ src/library/helpers/getSiteMode.js
@@ -2,6 +2,6 @@ import enumerations from "../../config/enumerations";
 
 export const getSiteMode = () => {
     const siteMode = localStorage.getItem('siteMode');
-    if (typeof siteMode === 'undefined' || siteMode === null) { return enumerations.SiteMode.Admin; }
+    if (typeof siteMode === 'undefined' || siteMode === null) { return enumerations.SiteMode.Normal; }
     return siteMode;
 };
